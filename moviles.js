3425c6875d42be954c1e262349c6adb66a73af5e
Miguel <miguelbogigante@gmail.com>
2020-07-11 16:46:28 +0200

    interfaz, archivos js y json

---
+++ moviles.js
@@ -0,0 +1,12 @@
+
+
+
+
+function myFunction(arr) {
+    var out = "";
+    var i;
+    for(i = 0; i < arr.length; i++) {
+        out += '<a href="' + arr[i].url + '">' + arr[i].display + '</a><br>';
+    }
+    document.getElementById("id01").innerHTML = out;
+}
