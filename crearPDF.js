e25cc182cdb4cae013e19a647c42df3bbb3312cf
Miguel <miguelbogigante@gmail.com>
2020-07-14 17:40:30 +0200

    Intento crear la funcion para contar voluntarios

--- crearPDF.js
+++ crearPDF.js
@@ -1,28 +1,43 @@
 document.getElementById("botonDescargar").addEventListener("click", descargarPDF);
 
+motivoServicio: String;
+fechaServicio: Date;
+numeroExpediente: String;
+municipio: String;
+horaEntrada: Date;
+horaSalida: Date;
+tipoActividad: String;
+voluntarios: Array;
+
 function descargarPDF() {
-    
-    motivoServicio: String;
-    fechaServicio: Date;
-    numeroExpediente: String;
-    municipio: String;
-    horaEntrada: Date;
-    horaSalida: Date;
-    tipoActividad: String;
-    
+
+
     motivoServicio = document.getElementById("motivoServicio").value;
 
-    console.log(motivoServicio);
+    // if(document.getElementById('checkBoxVoluntarios').ckecked){
 
+    //     voluntarios[0]
 
-    
-    
+    // }
+
+    getVoluntariosLenght();
+    console.log(numeroVoluntarios);
 
     tipoActividad = $("input:radio[name=radioTipoActividad]:checked").val();
 
-    console.log(tipoActividad);
-      
 
+    function getVoluntariosLenght() {
+
+        $.getJSON('voluntarios.json', function (data) {
+            
+            var numeroVoluntarios = Object.keys(data.voluntarios).length;
     
+        })
+    
+    }
+
 
 }
+
+
+
--- mostrarVoluntarios.js
+++ mostrarVoluntarios.js
@@ -4,28 +4,18 @@ $(function () {
 
     $.getJSON('voluntarios.json', function (data) {
         $.each(data.voluntarios, function (i, f) {
-            var tblRow = 
-            '<div style="margin-bottom:20px" class="input-group">' + 
-                '<div class="input-group-prepend">' + 
-                    '<div class="input-group-text">' + 
-                        '<input type="checkbox" aria-label="Checkbox for following text input">' +
-                    '</div>'+
-                '</div>' +
-                    '<input type="text" class="form-control" value="' + f.nombre + " " + f.indicativo + '" aria-label="Text input with checkbox">' + 
-            '</div>';
 
+            var tblRow =
+                '<div style="margin-bottom:20px" class="input-group">' +
+                '<div class="input-group-prepend">' +
+                '<div class="input-group-text">' +
+                '<input id="checkBoxVoluntarios" value="' + f.nombre + '" type="checkbox" aria-label="Checkbox for following text input">' +
+                '</div>' +
+                '</div>' +
+                '<input type="text" class="form-control" value="' + f.nombre + " " + f.indicativo + '" aria-label="Text input with checkbox">' +
+                '</div>';
 
             $(tblRow).appendTo("#userdata tbody");
         });
     });
 });
-
-
-
-    
-      
-    
-  
-  
-
-
